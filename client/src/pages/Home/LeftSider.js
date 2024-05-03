c1f487930f4cdf1816bc76c001f066c79e96e41b
HarshalAtre <113234343+HarshalAtre@users.noreply.github.com>
2024-05-03 13:59:04 +0530

    Update LeftSider.js

--- client/src/pages/Home/LeftSider.js
+++ client/src/pages/Home/LeftSider.js
@@ -13,7 +13,7 @@ function LeftSider() {
           <a href="mailto:example@example.com"><i className="ri-mail-line icon transition duration-300 transform hover:scale-110 hover:text-white" title="Mail"></i></a>
           <a href="https://www.instagram.com"><i className="ri-instagram-line icon transition duration-300 transform hover:scale-110 hover:text-white" title="Instagram"></i></a>
           <a href="https://www.linkedin.com"><i className="ri-linkedin-box-line icon transition duration-300 transform hover:scale-110 hover:text-white" title="LinkedIn"></i></a>
-          <a href="https://github.com"><i className="ri-github-fill icon transition duration-300 transform hover:scale-110 hover:text-white" title="GitHub"></i></a>     
+          <a href="https://github.com/HarshalAtre"><i className="ri-github-fill icon transition duration-300 transform hover:scale-110 hover:text-white" title="GitHub"></i></a>     
         </div>
         <div className='h-32 w-[1px] bg-[#2fa0a083] sm:hidden'></div>
       </div>
